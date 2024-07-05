fdad205a7f4143a39de6edb27c8854f19390a15b
DESKTOP-7TP68J3\PC <824314051@qq.com>
2024-07-05 09:16:42 +0800

    端口

--- app/Console/Commands/Workerman.php
+++ app/Console/Commands/Workerman.php
@@ -64,25 +64,25 @@ class Workerman extends Command
         $worker = new BusinessWorker();
         $worker->name = 'BusinessWorker';
         $worker->count = 1;
-        $worker->registerAddress = '127.0.0.1:1238';
+        $worker->registerAddress = '127.0.0.1:1236';
         $worker->eventHandler = \App\Workerman\Events::class;
     }
 
     private function startGateWay(): void
     {
-        $gateway = new Gateway("websocket://0.0.0.0:30006");
+        $gateway = new Gateway("websocket://0.0.0.0:2346");
         $gateway->name = 'Gateway';
         $gateway->count = 1;
         $gateway->lanIp = '127.0.0.1';
-        $gateway->startPort = 2900;
+        $gateway->startPort = 2300;
         $gateway->pingInterval = 30;
         $gateway->pingNotResponseLimit = 0;
         $gateway->pingData = '{"type":"@heart@"}';
-        $gateway->registerAddress = '127.0.0.1:1238';
+        $gateway->registerAddress = '127.0.0.1:1236';
     }
 
     private function startRegister(): void
     {
-        new Register('text://0.0.0.0:1238');
+        new Register('text://0.0.0.0:1236');
     }
 }
