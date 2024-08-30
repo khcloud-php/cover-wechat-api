22ed7b0931e6b07147e47ddaaf5af56b75794547
DESKTOP-7TP68J3\PC <824314051@qq.com>
2024-08-30 11:32:47 +0800

    补充命令行

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,8 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         //
         Commands\Workerman::class,
+        Commands\Assistant\FixData::class,
+        Commands\Friend\ApplyOverdue::class
     ];
 
     /**
